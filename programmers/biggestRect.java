c77b8aad0456a052efe40df0561c9951e2f8096c
seung-hun2 <jungsh2328@naver.com>
2022-12-09 14:46:19 +0900

    22.12.08 프로그래머스(가장 큰 정사각형 찾기)

--- programmers/biggestRect.java
+++ programmers/biggestRect.java
@@ -10,7 +10,7 @@ public class biggestRect {
         int answer = 0;
         int[][] arr = new int[board.length+1][board.length+1];
         for(int i=1;i<=board.length;i++){
-            for(int j=1;j<=board.length;j++){
+            for(int j=1;j<=board[0].length;j++){
                 if(board[i-1][j-1]!=0){
                     int min = Math.min(Math.min(arr[i-1][j],arr[i][j-1]),arr[i-1][j-1]);
                     arr[i][j] = min+1;
