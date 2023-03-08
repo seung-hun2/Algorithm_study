fe795d572197d12522c1570aa7149ee5bbff2eb8
seung-hun2 <jungsh2328@naver.com>
2023-03-08 23:02:08 +0900

    키패드 나누기

--- programmers/pushKeypad.java
+++ programmers/pushKeypad.java
@@ -5,24 +5,24 @@ public class pushKeypad {
         String answer = "";
         int lh=10,rh=12;
         for(int i=0;i<numbers.length;i++){
-            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){
+            if(numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7){          // 왼손
                 answer += "L";
-                lh = numbers[i];
+                lh = numbers[i];                                                // 왼손 위치 저장
             }
-            else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){
+            else if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){     // 오른손
                 answer += "R";
-                rh = numbers[i];
+                rh = numbers[i];                                                // 오른손 위치 저장
             }
             else{
-                if(numbers[i] == 0) numbers[i] = 11;
-                int rdis = Math.abs(numbers[i] - rh)/3 + Math.abs(numbers[i] - rh)%3;
-                int ldis = Math.abs(numbers[i] - lh)/3 + Math.abs(numbers[i] - lh)%3;
+                if(numbers[i] == 0) numbers[i] = 11;                            // 0을 11로 변경
+                int rdis = Math.abs(numbers[i] - rh)/3 + Math.abs(numbers[i] - rh)%3;   // 오른손 위치에서 다음 숫자까지의 거리
+                int ldis = Math.abs(numbers[i] - lh)/3 + Math.abs(numbers[i] - lh)%3;   // 왼손 위치에서 다음 숫자까지의 거리
                 if( ldis > rdis ){
-                    answer += "R";
-                    rh = numbers[i];
+                    answer += "R";                                          // 왼손이 더 멀면 오른손 추가하고
+                    rh = numbers[i];                                        // 오른손 위치 저장
                 }else if(rdis == ldis){
-                    if(hand.equals("right")){
-                        answer += "R";
+                    if(hand.equals("right")){                               // 거리가 같다면 오른손 잡이 -> 오른손 저장
+                        answer += "R";                                      // 왼손 잡이 -> 왼손 저장
                         rh = numbers[i];
                     }else{
                         answer += "L";
@@ -30,8 +30,8 @@ public class pushKeypad {
                     }
                 }
                 else{
-                    answer += "L";
-                    lh = numbers[i];
+                    answer += "L";                                          // 오른손이 더 멀면 왼손 추가하고
+                    lh = numbers[i];                                        // 왼손 위치 저장
                 }
             }
         }
