6d05775bcb2140ad21ebc7408765baee9ad2ff85
seung-hun2 <jungsh2328@naver.com>
2023-03-08 23:02:22 +0900

    문자열 나누기

--- programmers/splitString.java
+++ programmers/splitString.java
@@ -7,12 +7,12 @@ public class splitString {
         char c = ' ';
 
         for(int i=0;i<s.length();i++){
-            if(cnt == 0 ) {
+            if(cnt == 0 ) {                 //  남은 카운트가 없으면 들어 오는 알파벳 저장 후 정답 수 추가
                 c = s.charAt(i);
                 answer++;
             }
-            if(c == s.charAt(i)) cnt++;
-            else cnt--;
+            if(c == s.charAt(i)) cnt++;     // 들어오는 숫자가 저장된 알파벳과 같으면 카운트 추가
+            else cnt--;                     // 다르면 카운트 감소
 
         }
 
