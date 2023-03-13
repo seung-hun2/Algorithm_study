44ca56e2012616753f7e5054a4bdf763afba3517
seung-hun2 <jungsh2328@naver.com>
2023-03-13 23:13:43 +0900

    팩토리얼

---
+++ baekjoon/p10872.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = factorial(n);
+        System.out.println(sum);
+    }
+    public static int factorial(int n){
+        if(n<=1) return 1;
+        return factorial(n-1)*n;
+    }
+}
